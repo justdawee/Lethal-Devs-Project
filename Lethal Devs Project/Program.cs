49e8ad83cfb84c3eec06388bc706e04bf1108a45
Lukács Dávid <justdawee@gmail.com>
2022-04-08 10:47:06 +0200

    Adding MySQL connection

--- Lethal Devs Project/Program.cs
+++ Lethal Devs Project/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace Lethal_Devs_Project
 {
@@ -17,6 +18,15 @@ namespace Lethal_Devs_Project
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            MySqlConnection(@"server=localhost;userid=lethaldevs;password=secret;database=vms_lethaldevs");
+        }
+
+        static void MySqlConnection(string connection)
+        {
+            var mysqlcon = new MySqlConnection(connection);
+            mysqlcon.Open();
+
+            Console.WriteLine($"MySQL version : {mysqlcon.ServerVersion}");
         }
     }
 }
