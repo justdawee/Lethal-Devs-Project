55b768f67ec1224708f63a452c27d655ba6813ac
JustDawee <justdawee@gmail.com>
2022-04-23 14:04:37 +0200

    Removed userdata

--- VMS-SQL/vms_lethaldevs.sql
+++ VMS-SQL/vms_lethaldevs.sql
@@ -57,16 +57,6 @@ CREATE TABLE `users` (
   `email` varchar(255) COLLATE utf8_hungarian_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
---
--- A tábla adatainak kiíratása `users`
---
-
-INSERT INTO `users` (`id`, `username`, `password`, `name`, `admin`, `birthdate`, `address`, `regdate`, `lastvisit`, `phonenumber`, `email`) VALUES
-(1, 'JustDawee', 'jYIhvJFerKbES2+S9uloBQ==', 'Lukács Dávid', 1, '1999-06-03', '2344 Dömsöd, Bajcsy-Zsilinszky utca 14.', '2022-04-10 12:10:58', '2022-04-16 20:59:19', '06205961664', 'justdawee@gmail.com'),
-(9, 'MintaJanos', 'hzv735jRwZ5e/duHgl9wIg==', 'Minta János', 0, '2022-04-16', '1234 Budapest, Minta utca 12.', '2022-04-16 19:33:50', '2022-04-16 19:33:50', '06701234567', 'minta@gmail.com'),
-(10, 'MintaIstvan', 'hzv735jRwZ5e/duHgl9wIg==', 'MintaIstvan', 0, '2022-04-16', '1234 Budapest, Minta utca 12.', '2022-04-16 19:34:06', '2022-04-16 19:34:06', '06701234567', 'minta@gmail.com'),
-(11, 'MintaFerenc', 'hzv735jRwZ5e/duHgl9wIg==', 'MintaFerenc', 0, '2022-04-16', '1234 Budapest, Minta utca 12.', '2022-04-16 19:34:18', '2022-04-16 19:34:18', '06701234567', 'minta@gmail.com');
-
 -- --------------------------------------------------------
 
 --
@@ -88,16 +78,6 @@ CREATE TABLE `vehicles` (
   `added` datetime NOT NULL DEFAULT current_timestamp(),
   `active` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
-
---
--- A tábla adatainak kiíratása `vehicles`
---
-
-INSERT INTO `vehicles` (`vehid`, `owner`, `type`, `prodyear`, `vin`, `license`, `color`, `mileage`, `engine`, `ccm`, `fueltype`, `added`, `active`) VALUES
-(6, 'Lukács Dávid ', 'Audi A6', 2006, 'WVWXK73C78EXXXXXX', 'KRL264', 'Kék', '347987', 'I4', 1987, 'Dízel', '2022-04-14 23:06:34', 1),
-(9, 'Lukács Dávid ', 'Volkswagen Passat', 2016, 'WVWBA7AJXCWXXXXXX', 'ABC123', 'Fehér', '12532', 'I4', 1989, 'Dízel', '2022-04-15 10:55:26', 1),
-(10, 'Lukács Dávid', 'Nissan GT-R', 2008, 'WVWBA7AJXCWXXXXXX', 'ABC123', 'Fekete', '87622', 'V6', 2967, 'Benzin', '2022-04-15 10:57:48', 1),
-(14, 'Lukács Dávid ', 'Seat Corrado', 1996, 'WVWBA7AJXCWXXXXXX', 'ABC123', 'Sárga', '567899', 'I4', 1987, 'Dízel', '2022-04-15 13:27:43', 1);
 
 --
 -- Indexek a kiírt táblákhoz
