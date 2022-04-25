23ed6ff226c774775be5ef5bc85871b934a5f036
JustDawee <justdawee@gmail.com>
2022-04-25 21:51:14 +0200

    Home page statistics

--- VMS-Web/index.php
+++ VMS-Web/index.php
@@ -1,5 +1,28 @@
 <?php 
 include 'header.php';
+require_once 'config.php';
+
+//For vehicles and users statistics
+$targetuser = 10;
+$totalusers = "";
+$totalvehs = "";
+$avgmileage = "";
+
+$sql = "SELECT \n"
+    . "(SELECT COUNT(*) FROM users) AS usercount, \n"
+    . "(SELECT COUNT(*) FROM vehicles) AS vehcount, \n"
+    . "(SELECT AVG(mileage) FROM vehicles) AS avgmileage;";
+
+$result = $mysqli->query($sql);
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $totalusers = $row["usercount"];
+        $totalvehs = $row["vehcount"];
+        $avgmileage = $row["avgmileage"];
+    }
+}
+$result->close();
 
 ?>
 
@@ -14,7 +37,7 @@ include 'header.php';
                     <div class="row align-items-center no-gutters">
                         <div class="col me-2">
                             <div class="text-uppercase text-primary fw-bold text-xs mb-1"><span>Átlagos futás</span></div>
-                            <div class="text-dark fw-bold h5 mb-0"><span>159 000 km</span></div>
+                            <div class="text-dark fw-bold h5 mb-0"><span><?php echo number_format(round($avgmileage), 0, '.', ' ') . " km" ?></span></div>
                         </div>
                         <div class="col-auto"><i class="icon-speedometer fa-2x text-gray-300"></i></div>
                     </div>
@@ -27,7 +50,7 @@ include 'header.php';
                     <div class="row align-items-center no-gutters">
                         <div class="col me-2">
                             <div class="text-uppercase text-success fw-bold text-xs mb-1"><span>Járművek</span></div>
-                            <div class="text-dark fw-bold h5 mb-0"><span>15 db</span></div>
+                            <div class="text-dark fw-bold h5 mb-0"><span><?php echo $totalvehs . " db" ?></span></div>
                         </div>
                         <div class="col-auto"><i class="fas fa-car-side fa-2x text-gray-300"></i></div>
                     </div>
@@ -42,11 +65,11 @@ include 'header.php';
                             <div class="text-uppercase text-info fw-bold text-xs mb-1"><span>Cél felhasználó</span></div>
                             <div class="row g-0 align-items-center">
                                 <div class="col-auto">
-                                    <div class="text-dark fw-bold h5 mb-0 me-3"><span>50%</span></div>
+                                    <div class="text-dark fw-bold h5 mb-0 me-3"><span><?php echo $totalusers / $targetuser * 100 . "%" ?></span></div>
                                 </div>
                                 <div class="col">
                                     <div class="progress progress-sm">
-                                        <div class="progress-bar bg-info" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 50%;"><span class="visually-hidden">50%</span></div>
+                                        <div class="progress-bar bg-info" aria-valuenow="<?php echo $totalusers ?>" aria-valuemin="0" aria-valuemax="<?php echo $targetuser ?>" style="width: <?php echo $totalusers / $targetuser * 100 . "%" ?>;"><span class="visually-hidden"><?php echo $totalusers / $targetuser * 100 . "%" ?></span></div>
                                     </div>
                                 </div>
                             </div>
@@ -62,7 +85,7 @@ include 'header.php';
                     <div class="row align-items-center no-gutters">
                         <div class="col me-2">
                             <div class="text-uppercase text-warning fw-bold text-xs mb-1"><span>Felhasználók</span></div>
-                            <div class="text-dark fw-bold h5 mb-0"><span>18</span></div>
+                            <div class="text-dark fw-bold h5 mb-0"><span><?php echo $totalusers . " db" ?></span></div>
                         </div>
                         <div class="col-auto"><i class="fas fa-user fa-2x text-gray-300"></i></div>
                     </div>
@@ -74,36 +97,37 @@ include 'header.php';
         <div class="col-lg-7 col-xl-8">
             <div class="card shadow mb-4">
                 <div class="card-header d-flex justify-content-between align-items-center">
-                    <h6 class="text-primary fw-bold m-0">Legnagyobb járművek</h6>
+                    <h6 class="text-primary fw-bold m-0">Legnagyobb járművek cm<sup>3</sup> alapján</h6>
                     <div class="dropdown no-arrow"><button class="btn btn-link btn-sm dropdown-toggle" aria-expanded="false" data-bs-toggle="dropdown" type="button"><i class="fas fa-ellipsis-v text-gray-400"></i></button>
                     </div>
                 </div>
                 <div class="card-body text-break text-uppercase">
                     <div class="table-responsive">
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th>Típus</th>
-                                    <th>MOTOR</th>
-                                    <th>KÖBCENTI</th>
-                                    <th>TULAJ</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>2006 Audi a6</td>
-                                    <td>V8</td>
-                                    <td>5896</td>
-                                    <td>Minta Ferenc</td>
-                                </tr>
-                                <tr>
-                                    <td>1987 Chevrolet Camaro SS</td>
-                                    <td>V8</td>
-                                    <td>7892</td>
-                                    <td>JOHN DIAZ</td>
-                                </tr>
-                            </tbody>
-                        </table>
+                    <?php
+                    $conn = $mysqli;
+                        $result = mysqli_query($conn,"SELECT prodyear, type, engine, ccm, owner FROM vehicles ORDER BY ccm DESC");
+
+                        echo "<table class='table'>
+                        <thead>
+                        <tr>
+                        <th>Típus</th>
+                        <th>Motor</th>
+                        <th>cm<sup>3</sup></th>
+                        <th>Tulaj</th>
+                        </tr>
+                        </thead>";
+                        
+                        while($row = mysqli_fetch_array($result))
+                        {
+                        echo "<tr>";
+                        echo "<td>" . $row['prodyear'] . " " . $row['type'] . "</td>";
+                        echo "<td>" . $row['engine'] . "</td>";
+                        echo "<td>" . $row['ccm'] . "</td>";
+                        echo "<td>" . $row['owner'] . "</td>";
+                        echo "</tr>";
+                        }
+                        echo "</table>";
+                    ?>
                     </div>
                 </div>
             </div>
