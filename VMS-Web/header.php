94c30cf42ea1ce8a828d530020addb9f49bd9c4c
JustDawee <justdawee@gmail.com>
2022-04-28 13:27:14 +0200

    Bugfixes, changed title

--- VMS-Web/header.php
+++ VMS-Web/header.php
@@ -16,7 +16,7 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Dashboard - Brand</title>
+    <title>VMS - Lethal Devs</title>
     <link rel="apple-touch-icon" type="image/png" sizes="180x180" href="assets/img/favicon-180x180.png">
     <link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicon-16x16.png">
     <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicon-32x32.png">
